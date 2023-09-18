69d6e71cb270465c032114e56647eab20c1834e5
Douglas dos Santos Oliveira <doug1306@gmail.com>
2023-09-18 15:07:59 -0300

    feat: end project

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
+
+const { CLIENT_ID } = process.env
 
 const api = axios.create({
   baseURL: 'https://api.twitch.tv/helix'
 })
+
 
 export { api };
